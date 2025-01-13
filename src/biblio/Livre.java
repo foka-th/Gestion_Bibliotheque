d7645418e4faffc64a397816b03d41117ff19979
Foka S. Thierry <thierryfoka32@gmail.com>
2025-01-13 17:12:03 +0100

    une autre modification de la classe Livre

--- src/biblio/Livre.java
+++ src/biblio/Livre.java
@@ -1,5 +1,7 @@
 package biblio;
 
+import java.time.LocalDate;
+
 public class Livre {
     private int id;
     private String isbn;
@@ -7,16 +9,18 @@ public class Livre {
     private String auteur;
     private String nationaliteAuteur;
     private String categorie;
+    private LocalDate dateParution;
     private int nombreExemplaires;
 
     // Constructeurs
-    public Livre(int id, String isbn, String titre, String auteur, String nationaliteAuteur, String categorie, int nombreExemplaires) {
+    public Livre(int id, String isbn, String titre, String auteur, String nationaliteAuteur, String categorie, LocalDate dateParution, int nombreExemplaires) {
         this.id = id;
         this.isbn = isbn;
         this.titre = titre;
         this.auteur = auteur;
         this.nationaliteAuteur = nationaliteAuteur;
         this.categorie = categorie;
+        this.dateParution = dateParution;
         this.nombreExemplaires = nombreExemplaires;
     }
 
@@ -33,6 +37,8 @@ public class Livre {
     public void setNationaliteAuteur(String nationaliteAuteur) { this.nationaliteAuteur = nationaliteAuteur; }
     public String getCategorie() { return categorie; }
     public void setCategorie(String categorie) { this.categorie = categorie; }
+    public LocalDate getDateParution() { return dateParution; }
+    public void setDateParution(LocalDate dateParution) { this.dateParution = dateParution; }
     public int getNombreExemplaires() { return nombreExemplaires; }
     public void setNombreExemplaires(int nombreExemplaires) { this.nombreExemplaires = nombreExemplaires; }
 
